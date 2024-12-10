893891c579d991ad044d7d920562ef304a509052
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-10 15:02:52 +0530

    Add files via upload

---
+++ Val.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Val
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n==121)
+		System.out.println("likely");
+		else
+		System.out.println("unlikely");
+
+	}
+}
+/*  121
+likely */
