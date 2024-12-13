93e42a4fcd8d59fd9655cd856b3f3ef55afd85ef
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-13 19:03:16 +0530

    Add files via upload

---
+++ Practice.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Practice
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int p1=sc.nextInt();
+		int p2=sc.nextInt();
+		int p3=sc.nextInt();
+		int p4=sc.nextInt();
+		int count=0;
+		if(p1>=10)
+		count++;
+		if(p2>=10)
+		count++;
+		if(p3>=10)
+		count++;
+		if(p4>=10)
+		count++;
+		System.out.println(count);
+
+	}
+}
+/* 12 15 8 10
+3*/
