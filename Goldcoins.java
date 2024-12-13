e28629ec76df5202ab55a36a223ca22ea1129f07
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-13 19:11:22 +0530

    Add files via upload

---
+++ Goldcoins.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Goldcoins
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int A=sc.nextInt();
+	    int B=sc.nextInt();
+	    int X=sc.nextInt();
+	    int Y=sc.nextInt();
+	    if(X>=Y)
+	    System.out.println(A);
+	    else
+	    System.out.println(B);
+		
+
+	}
+}
+/* 5 2 3 4
+2*/
