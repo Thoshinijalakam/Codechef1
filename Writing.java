f43f7b2b37a4059e0d9e5c44a4f0b00303111777
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-13 18:04:52 +0530

    Add files via upload

---
+++ Writing.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Writing
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int X=sc.nextInt();
+	    if(5*X<=60)
+        System.out.println("yes");
+        else
+        System.out.println("No");
+	}
+}
+/* 5
+yes*/
