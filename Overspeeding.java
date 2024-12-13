5365210fb415a84ed79bce5c6675a67249a71b4d
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-13 19:08:24 +0530

    Add files via upload

---
+++ Overspeeding.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Overspeeding
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int s=sc.nextInt();
+		if(s>40)
+		System.out.println("yes");
+		else 
+		System.out.println("no");
+
+	}
+}
+/* 40
+no*/
