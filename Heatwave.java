b1a179f367d56ffe38c3c6fb2691a4a6929a36f7
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-13 19:09:50 +0530

    Add files via upload

---
+++ Heatwave.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Heatwave
+{
+	public static void main (String[] args){
+	    Scanner sc=new Scanner(System.in);
+	    int x=sc.nextInt();
+	    int y=sc.nextInt();
+	    if(y>x)
+	    System.out.println("yes");
+	    else 
+	    System.out.println("no");
+
+	}
+}
+/* 135 110
+no*/
