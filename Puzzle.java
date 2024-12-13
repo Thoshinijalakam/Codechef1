26b5d2aee7a1cac5674749c633c4433c2c671a8b
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-13 19:00:45 +0530

    Add files via upload

---
+++ Puzzle.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Puzzle
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		if(N>=6&&N<=8)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+
+	}
+}
+/* 4
+no*/
