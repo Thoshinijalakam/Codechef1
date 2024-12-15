201344301f55cdc0f43a0df91be28e45c825ccec
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-15 11:43:59 +0530

    Add files via upload

---
+++ Summertime.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Summertime
+
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		if(x>35)
+		System.out.println("yes");
+		else
+		System.out.println("No");
+		
+
+	}
+}
+/*35 
+No*/
