1d896f8016c67183cce99b0ee29a37e04cd521c6
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-15 11:47:21 +0530

    Add files via upload

---
+++ Nugget.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Nugget
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int x=sc.nextInt();
+	    int y=sc.nextInt();
+	    System.out.println(x*5000+y*9800);
+	}
+}
+/* 2 3
+39400*/
