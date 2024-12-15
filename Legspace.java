2f55a8aa2a95e5e97982d0172a6f236b787b53cd
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-15 11:49:25 +0530

    Add files via upload

---
+++ Legspace.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Legspace
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    int m=sc.nextInt();
+	    if(n<m)
+	    System.out.println("yes");
+	    else
+	    System.out.println("no");
+
+	}
+}
